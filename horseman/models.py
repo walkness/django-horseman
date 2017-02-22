7d2f8380ca8bfe6a07a51d15b72761583688afd7
Walker Angell <w@lker.co>
2017-02-22 12:07:04 -0500

    initial setup

---
+++ horseman/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Node(models.Model):
+
+    class Meta:
+        abstract = True
---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+long_description = open('README.rst').read()
+
+setup(
+    name='django-horseman',
+    version='0.1',
+    description='A simple headless CMS for Django',
+    long_description=long_description,
+    author='Walker Angell',
+    author_email='w@lker.co',
+    url='http://github.com/sunlightlabs/django-wordpress/',
+    packages=['horseman'],
+    classifiers=[],
+    license='MIT',
+    platforms=['any'],
+)
