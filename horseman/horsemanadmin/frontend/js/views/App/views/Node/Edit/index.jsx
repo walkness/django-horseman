f3f6954082c3606dcc1ccd5f8e69f363225d3e7d
Walker Angell <w@lker.co>
2017-10-28 13:24:13 -0400

    set changed to false on successful save

--- horseman/horsemanadmin/frontend/js/views/App/views/Node/Edit/index.jsx
+++ horseman/horsemanadmin/frontend/js/views/App/views/Node/Edit/index.jsx
@@ -132,7 +132,7 @@ class EditNode extends Component {
         } else {
           const action = params.id ? this.props.nodeUpdated : this.props.nodeCreated;
           action(response, nodeType);
-          this.setState({ saving: false, error: null });
+          this.setState({ saving: false, error: null, changed: false });
           if (!params.id) {
             const { adminURLBase } = this.props;
             const { app_label, model_name } = nodeState.configuration;
