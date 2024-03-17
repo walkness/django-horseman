e57f5ccb417c5047631a5550d1c86ef1dadbe84d
Walker Angell <w@lker.co>
2024-03-17 11:59:21 -0400

    refactor to build as tar package on GitHub and include compiled frontend as static assets

--- setup.py
+++ setup.py
@@ -1,27 +1,19 @@
 from setuptools import setup, find_packages
 
-long_description = open('README.rst').read()
+long_description = open("README.rst").read()
 
 setup(
-    name='django-horseman',
-    version='0.1',
-    description='A simple headless CMS for Django',
+    name="django-horseman",
+    version="0.1",
+    description="A simple headless CMS for Django",
     long_description=long_description,
-    author='Walker Angell',
-    author_email='w@lker.co',
-    url='https://github.com/walkness/django-horseman',
-    packages=[
-        'horseman',
-        'horseman.horsemanadmin',
-        'horseman.horsemanapi',
-        'horseman.horsemancomments',
-        'horseman.horsemanfrontendcache',
-        'horseman.horsemanimages',
-        'horseman.horsemannodes',
-        'horseman.horsemanoptions',
-        'horseman.horsemanusers',
-    ],
+    author="Walker Angell",
+    author_email="w@lker.co",
+    url="https://github.com/walkness/django-horseman",
+    packages=find_packages(include=["horseman", "horseman.*"]),
+    package_data={"horseman.horsemanadmin": ["templates/**/*", "static/**/*"]},
+    include_package_data=True,
     classifiers=[],
-    license='MIT',
-    platforms=['any'],
+    license="MIT",
+    platforms=["any"],
 )
