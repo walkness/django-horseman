95cbe532ada19aeb8e962f3e16fa5b760502f68f
Walker Angell <w@lker.co>
2024-03-17 15:41:58 -0400

    WIP

--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
 from setuptools import setup, find_packages
 
-long_description = open('README.rst').read()
+long_description = open("README.rst").read()
 
 setup(
-    name='django-horseman',
-    version='0.1',
-    description='A simple headless CMS for Django',
+    name="django-horseman",
+    version="0.1",
+    description="A simple headless CMS for Django",
     long_description=long_description,
-    author='Walker Angell',
-    author_email='w@lker.co',
-    url='https://github.com/walkness/django-horseman',
-    packages=find_packages(include=['horseman', 'horseman.*']),
-    package_data={'horseman.horsemanadmin': ['templates/**/*', 'frontend/dist/**/*']},
-    include_package_data=True,
+    author="Walker Angell",
+    author_email="w@lker.co",
+    url="https://github.com/walkness/django-horseman",
+    package_data={"horseman.horsemanadmin": ["templates/**/*", "frontend/dist/**/*"]},
     classifiers=[],
-    license='MIT',
-    platforms=['any'],
+    license="MIT",
+    platforms=["any"],
 )
